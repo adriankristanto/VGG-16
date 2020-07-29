cf1135cf75e1c3575b1bca29b424e2cb0e19a27e
Adrian Kristanto <49110670+adriankristanto@users.noreply.github.com>
2020-07-29 16:30:13 +1000

    reimplemented vgg16 model from scratch

--- code/VGG16.py
+++ code/VGG16.py
@@ -2,21 +2,18 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F 
 
-# VGG16 configuration
-CONV_KERNEL_SIZE = (3, 3)
-CONV_STRIDE = (1, 1)
-POOL_KERNEL_SIZE = (2, 2)
-POOL_STRIDE = (2, 2)
-
-def p(n, f, s, out):
-    """
-    n: input size
-    f: kernel/filter size
-    s: stride
-    out: output size
-    """
-    # out = (n + 2p -f)/s + 1
-    return (s * (out - 1) - n + f)//2
+# padding calculation for CONV layer as POOL layer doesn't require padding
+# each CONV has the same configuration
+# s = 1, f = 3 and we want 'same' padding
+# therefore,
+"""
+p = (s(out-1)-n+f) / 2
+p = (out-1-n+f) / 2
+same padding means n == out
+p = (n-1-n+f) / 2
+p = (f-1)/2
+p = (3-1)/2 = 1
+"""
 
 class Net(nn.Module):
 
@@ -26,118 +23,77 @@ class Net(nn.Module):
         num_classes: int, total number of classes that an input image may belong to, by default it is set to 1000
         """
         super(Net, self).__init__()
-        self.features = nn.Sequential(
-            # CONV1
-            # input size: input_size * input_size * 3
-            # output size: input_size * input_size * 64
-            nn.Conv2d(in_channels=3, out_channels=64, kernel_size=CONV_KERNEL_SIZE, stride=CONV_STRIDE, padding=p(input_size, CONV_KERNEL_SIZE[0], CONV_STRIDE[0], input_size)),
-            nn.ReLU(),
-            # CONV2
-            # input size: input_size * input_size * 64
-            # output size: input_size * input_size * 64
-            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=CONV_KERNEL_SIZE, stride=CONV_STRIDE, padding=p(input_size, CONV_KERNEL_SIZE[0], CONV_STRIDE[0], input_size)),
-            nn.ReLU(),
-            # POOL2
-            # input size: input_size * input_size * 64
-            # output size: input_size/2 * input_size/2 * 64
-            nn.MaxPool2d(kernel_size=POOL_KERNEL_SIZE, stride=POOL_STRIDE),
-            # CONV3
-            # input size: input_size/2 * input_size/2 * 64
-            # output size: input_size/2 * input_size/2 * 128
-            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=CONV_KERNEL_SIZE, stride=CONV_STRIDE, padding=p(input_size, CONV_KERNEL_SIZE[0], CONV_STRIDE[0], input_size)),
-            nn.ReLU(),
-            # CONV4
-            # input size: input_size/2 * input_size/2 * 128
-            # output size: input_size/2 * input_size/2 * 128
-            nn.Conv2d(in_channels=128, out_channels=128, kernel_size=CONV_KERNEL_SIZE, stride=CONV_STRIDE, padding=p(input_size, CONV_KERNEL_SIZE[0], CONV_STRIDE[0], input_size)),
-            nn.ReLU(),
-            # POOL4
-            # input size: input_size/2 * input_size/2 * 128
-            # output size: input_size/4 * input_size/4 * 128
-            nn.MaxPool2d(kernel_size=POOL_KERNEL_SIZE, stride=POOL_STRIDE),
-            # CONV5
-            # input size: input_size/4 * input_size/4 * 128
-            # output size: input_size/4 * input_size/4 * 256
-            nn.Conv2d(in_channels=128, out_channels=256, kernel_size=CONV_KERNEL_SIZE, stride=CONV_STRIDE, padding=p(input_size, CONV_KERNEL_SIZE[0], CONV_STRIDE[0], input_size)),
-            nn.ReLU(),
-            # CONV6
-            # input size: input_size/4 * input_size/4 * 256
-            # output size: input_size/4 * input_size/4 * 256
-            nn.Conv2d(in_channels=256, out_channels=256, kernel_size=CONV_KERNEL_SIZE, stride=CONV_STRIDE, padding=p(input_size, CONV_KERNEL_SIZE[0], CONV_STRIDE[0], input_size)),
-            nn.ReLU(),
-            # CONV7
-            # input size: input_size/4 * input_size/4 * 256
-            # output size: input_size/4 * input_size/4 * 256
-            nn.Conv2d(in_channels=256, out_channels=256, kernel_size=CONV_KERNEL_SIZE, stride=CONV_STRIDE, padding=p(input_size, CONV_KERNEL_SIZE[0], CONV_STRIDE[0], input_size)),
-            nn.ReLU(),
-            # POOL7
-            # input size: input_size/4 * input_size/4 * 256
-            # output size: input_size/8 * input_size/8 * 256
-            nn.MaxPool2d(kernel_size=POOL_KERNEL_SIZE, stride=POOL_STRIDE),
-            # CONV8
-            # input size: input_size/8 * input_size/8 * 256
-            # output size: input_size/8 * input_size/8 * 512
-            nn.Conv2d(in_channels=256, out_channels=512, kernel_size=CONV_KERNEL_SIZE, stride=CONV_STRIDE, padding=p(input_size, CONV_KERNEL_SIZE[0], CONV_STRIDE[0], input_size)),
-            nn.ReLU(),
-            # CONV9
-            # input size: input_size/8 * input_size/8 * 512
-            # output size: input_size/8 * input_size/8 * 512
-            nn.Conv2d(in_channels=512, out_channels=512, kernel_size=CONV_KERNEL_SIZE, stride=CONV_STRIDE, padding=p(input_size, CONV_KERNEL_SIZE[0], CONV_STRIDE[0], input_size)),
-            nn.ReLU(),
-            # CONV10
-            # input size: input_size/8 * input_size/8 * 512
-            # output size: input_size/8 * input_size/8 * 512
-            nn.Conv2d(in_channels=512, out_channels=512, kernel_size=CONV_KERNEL_SIZE, stride=CONV_STRIDE, padding=p(input_size, CONV_KERNEL_SIZE[0], CONV_STRIDE[0], input_size)),
-            nn.ReLU(),
-            # POOL10
-            # input size: input_size/8 * input_size/8 * 512
-            # output size: input_size/16 * input_size/16 * 512
-            nn.MaxPool2d(kernel_size=POOL_KERNEL_SIZE, stride=POOL_STRIDE),
-            # CONV11
-            # input size: input_size/16 * input_size/16 * 512
-            # output size: input_size/16 * input_size/16 * 512
-            nn.Conv2d(in_channels=512, out_channels=512, kernel_size=CONV_KERNEL_SIZE, stride=CONV_STRIDE, padding=p(input_size, CONV_KERNEL_SIZE[0], CONV_STRIDE[0], input_size)),
-            nn.ReLU(),
-            # CONV12
-            # input size: input_size/16 * input_size/16 * 512
-            # output size: input_size/16 * input_size/16 * 512
-            nn.Conv2d(in_channels=512, out_channels=512, kernel_size=CONV_KERNEL_SIZE, stride=CONV_STRIDE, padding=p(input_size, CONV_KERNEL_SIZE[0], CONV_STRIDE[0], input_size)),
-            nn.ReLU(),
-            # CONV13
-            # input size: input_size/16 * input_size/16 * 512
-            # output size: input_size/16 * input_size/16 * 512
-            nn.Conv2d(in_channels=512, out_channels=512, kernel_size=CONV_KERNEL_SIZE, stride=CONV_STRIDE, padding=p(input_size, CONV_KERNEL_SIZE[0], CONV_STRIDE[0], input_size)),
-            nn.ReLU(),
-            # POOL13
-            # input size: input_size/16 * input_size/16 * 512
-            # output size: input_size/32 * input_size/32 * 512
-            nn.MaxPool2d(kernel_size=POOL_KERNEL_SIZE, stride=POOL_STRIDE)
-        )
-        self.classifier = nn.Sequential(
-            # FC14
-            # input size: input_size/32 * input_size/32 * 512
-            # output size: 4096
-            # Activation: ReLU
-            nn.Linear(input_size//32 * input_size//32 * 512, 4096),
-            nn.ReLU(),
-            # FC15
-            # input size: 4096
-            # output size: 4096
-            # Activation: ReLU
-            nn.Linear(4096, 4096),
-            nn.ReLU(),
-            # FC16
-            # input size: 4096
-            # output size: num_classes
-            # Activation: softmax
-            nn.Linear(4096, num_classes),
-            nn.Softmax(dim=1)
-        )
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=64, kernel_size=(3,3), stride=(1,1), padding=1)
+        self.conv2 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=(3,3), stride=(1,1), padding=1)
+        
+        self.conv3 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=(3,3), stride=(1,1), padding=1)
+        self.conv4 = nn.Conv2d(in_channels=128, out_channels=128, kernel_size=(3,3), stride=(1,1), padding=1)
+
+        self.conv5 = nn.Conv2d(in_channels=128, out_channels=256, kernel_size=(3,3), stride=(1,1), padding=1)
+        self.conv6 = nn.Conv2d(in_channels=256, out_channels=256, kernel_size=(3,3), stride=(1,1), padding=1)
+        self.conv7 = nn.Conv2d(in_channels=256, out_channels=256, kernel_size=(3,3), stride=(1,1), padding=1)
+
+        self.conv8 = nn.Conv2d(in_channels=256, out_channels=512, kernel_size=(3,3), stride=(1,1), padding=1)
+        self.conv9 = nn.Conv2d(in_channels=512, out_channels=512, kernel_size=(3,3), stride=(1,1), padding=1)
+        self.conv10 = nn.Conv2d(in_channels=512, out_channels=512, kernel_size=(3,3), stride=(1,1), padding=1)
+
+        self.conv11 = nn.Conv2d(in_channels=512, out_channels=512, kernel_size=(3,3), stride=(1,1), padding=1)
+        self.conv12 = nn.Conv2d(in_channels=512, out_channels=512, kernel_size=(3,3), stride=(1,1), padding=1)
+        self.conv13 = nn.Conv2d(in_channels=512, out_channels=512, kernel_size=(3,3), stride=(1,1), padding=1)
+
+        conv_output = (input_size // 32) * (input_size // 32) * 512
+        self.fc14 = nn.Linear(conv_output, 4096)
+        self.fc15 = nn.Linear(4096, 4096)
+        self.fc16 = nn.Linear(4096, num_classes)
+
+        self.pool = nn.MaxPool2d(kernel_size=(2,2), stride=(2,2))
 
     def forward(self, x):
-        x = self.features(x)
+        # CONV1 -> ReLU -> CONV2 -> ReLU -> POOL
+        x = self.conv1(x)
+        x = F.relu(x)
+        x = self.conv2(x)
+        x = F.relu(x)
+        x = self.pool(x)
+        # CONV3 -> ReLU -> CONV4 -> ReLU -> POOL
+        x = self.conv3(x)
+        x = F.relu(x)
+        x = self.conv4(x)
+        x = F.relu(x)
+        x = self.pool(x)
+        # CONV5 -> ReLU -> CONV6 -> ReLU -> CONV7 -> ReLU -> POOL
+        x = self.conv5(x)
+        x = F.relu(x)
+        x = self.conv6(x)
+        x = F.relu(x)
+        x = self.conv7(x)
+        x = F.relu(x)
+        x = self.pool(x)
+        # CONV8 -> ReLU -> CONV9 -> ReLU -> CONV10 -> ReLU -> POOL
+        x = self.conv8(x)
+        x = F.relu(x)
+        x = self.conv9(x)
+        x = F.relu(x)
+        x = self.conv10(x)
+        x = F.relu(x)
+        x = self.pool(x)
+        # CONV11 -> ReLU -> CONV12 -> ReLU -> CONV13 -> ReLU -> POOL
+        x = self.conv11(x)
+        x = F.relu(x)
+        x = self.conv12(x)
+        x = F.relu(x)
+        x = self.conv13(x)
+        x = F.relu(x)
+        x = self.pool(x)
+        # Flatten
         x = torch.flatten(x, start_dim=1)
-        x = self.classifier(x)
+        # FC14 -> ReLU -> FC15 -> ReLU -> FC16 -> Softmax
+        x = self.fc14(x)
+        x = F.relu(x)
+        x = self.fc15(x)
+        x = F.relu(x)
+        x = self.fc16(x)
+        x = F.softmax(x, dim=1)
         return x
 
 
